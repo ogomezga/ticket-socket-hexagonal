a041dab2cf76cbf40d8e2c02afae3c7aceb01e08
ogomezga <osckar96@gmail.com>
2023-02-21 19:58:30 +0100

    feat(master): create index

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { ServerFactory } from "./infrastructure/server-factory"
+
+const serverFactory = new ServerFactory({ port: 3000});
+
+serverFactory
+.createServer()
+.then(async() => await serverFactory.start)
+.then(() => console.info('Server started: ', serverFactory.getConfig()))
+.catch((reason) => console.error('Error en in server: ', reason));
