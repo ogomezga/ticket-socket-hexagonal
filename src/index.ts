7c86801c91d709a37281e9b004fbe01c0df6e6b1
ogomezga <osckar96@gmail.com>
2023-02-22 19:25:01 +0100

    fix(master): unnecessary await

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ const serverFactory = new ServerFactory({ port: 3000});
 
 serverFactory
 .createServer()
-.then(async() => await serverFactory.start)
+.then(async() => serverFactory.start())
 .then(() => console.info('Server started: ', serverFactory.getConfig()))
 .catch((reason) => console.error('Error en in server: ', reason));
