21fc6a230b1b92c361a73ea23f6105b28deaa3dd
ogomezga <osckar96@gmail.com>
2023-04-02 20:48:50 +0200

    test(master): adapting the test to the new crud behaviour

--- src/test/infrastructure/repositories/ticket-handler-repository.test.ts
+++ src/test/infrastructure/repositories/ticket-handler-repository.test.ts
@@ -1,4 +1,4 @@
-import { Ticket } from '../../../domain/ticket';
+import { TicketHandlerInfo } from '../../../domain/models/ticket-handler-info';
 import { TicketRepository } from '../../../infrastructure/repositories/ticket-handler-repository';
 import fs from 'fs';
 
@@ -12,6 +12,8 @@ describe('Test suite for the ticket handler repository', () => {
         }) as unknown as Date);
     }
 
+    const dbConnectionPath = 'D:\\Developer\\ProyectosNode\\ticket-socket-hexagonal\\db\\data.json';
+
     beforeEach(() => {
         fs.writeFileSync = jest.fn();
         fs.readFileSync = jest.fn().mockImplementation(() => ({
@@ -22,58 +24,38 @@ describe('Test suite for the ticket handler repository', () => {
         }));
     });
 
-    test('Should return all the current information manage by the ticket handler', () => {
-        // Given
-        mockDate();
-        const ticketHandlerRepository = new TicketRepository();
-
-        // When
-        const result = ticketHandlerRepository.toJson();
-
-        // Then
-        const expectedCurrentTicketHandlerInfo = {
-            latestTicket: 0,
-            today: 13,
-            tickets: [],
-            lastFourTickets: [],
-        };
-        expect(result).toEqual(expectedCurrentTicketHandlerInfo);
-    });
-
     describe('Givena  request to init the repository', () => {
+        test('TicketRepository must be a class that can be instantiated', () => {
+            // Given & When
+            const ticketRepository = new TicketRepository();
+
+            // Then
+            expect(ticketRepository).toBeInstanceOf(TicketRepository);
+        });
+
         it('Should start the repository with the latest status available for the current day', () => {
             // Given
             mockDate();
-            const ticketHandlerRepository = new TicketRepository();
 
             // When
-            ticketHandlerRepository.init();
+            new TicketRepository();
 
             // Then
-            const expectedInitTicketHandlerInfo = {
-                latestTicket: 0,
-                today: 13,
-                tickets: [],
-                lastFourTickets: [],
-
-            };
-            const result = ticketHandlerRepository.toJson();
-            expect(result).toEqual(expectedInitTicketHandlerInfo);
+            expect(fs.writeFileSync).not.toHaveBeenCalled();
         });
 
         it('Should start the repository from scratch as it is a new day', () => {
             // Given
             mockDate(20);
-            const ticketHandlerRepository = new TicketRepository();
             fs.readFileSync = jest.fn().mockImplementation(() => ({
                 latestTicket: 0,
-                today: 20,
+                today: 13,
                 tickets: [],
                 lastFourTickets: [],
             }));
 
             // When
-            ticketHandlerRepository.init();
+            new TicketRepository();
 
             // Then
             const expectedInitTicketHandlerInfo = {
@@ -81,78 +63,109 @@ describe('Test suite for the ticket handler repository', () => {
                 today: 20,
                 tickets: [],
                 lastFourTickets: [],
-
             };
-            const result = ticketHandlerRepository.toJson();
-            expect(result).toEqual(expectedInitTicketHandlerInfo);
+            expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
+            expect(fs.writeFileSync).toHaveBeenNthCalledWith(1, dbConnectionPath, JSON.stringify(expectedInitTicketHandlerInfo));
         });
     });
 
-    it('Should save the current status of the ticket hundler repository', () => {
+    test('Given a request to save the ticket handler\'s info, it must save the informed data in the repository', () => {
+        // Given
+        mockDate();
+        const ticketHandlerRepository = new TicketRepository();
+        const expectedTicketHandlerInfo = {
+            latestTicket: 0,
+            today: 20,
+            tickets: [],
+            lastFourTickets: [],
+        };
+
+        // When
+        ticketHandlerRepository.saveCurrentTicketHandlerInformation(new TicketHandlerInfo(expectedTicketHandlerInfo));
+
+        // Then
+        expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
+        expect(fs.writeFileSync).toHaveBeenNthCalledWith(1, dbConnectionPath, JSON.stringify(expectedTicketHandlerInfo));
+    });
+
+    test('Given a request to read the ticket handler\'s info, it must read the data from the repository', () => {
         // Given
         mockDate();
         const ticketHandlerRepository = new TicketRepository();
 
         // When
-        ticketHandlerRepository.saveCurrentTicketHandlerInformation();
+        const result = ticketHandlerRepository.readCurrentTicketHandlerInformation();
 
         // Then
-        expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
+        const expectedTicketHandlerInfo = {
+            latestTicket: 0,
+            today: 13,
+            tickets: [],
+            lastFourTickets: [],
+        };
+        expect(fs.readFileSync).toHaveBeenNthCalledWith(1, dbConnectionPath, 'utf8');
+        expect(result).toEqual(expectedTicketHandlerInfo);
     });
+});
 
-    it('Should add a new ticket to the repository', () => {
+/*
+test('Should return all the current information manage by the ticket handler', () => {
+    // Given
+    mockDate();
+    const ticketHandlerRepository = new TicketRepository();
+
+    // When
+    const result = ticketHandlerRepository.toJson();
+
+    // Then
+    const expectedCurrentTicketHandlerInfo = {
+        latestTicket: 0,
+        today: 13,
+        tickets: [],
+        lastFourTickets: [],
+    };
+    expect(result).toEqual(expectedCurrentTicketHandlerInfo);
+});
+
+
+describe('Given a request to service a ticket', () => {
+    it('Should return a ticket with an assigned desk', () => {
         // Given
+        const desktop = 0;
+        mockDate();
+        fs.readFileSync = jest.fn().mockImplementation(() => ({
+            latestTicket: 0,
+            today: 13,
+            tickets: [{
+                numb: 1,
+                desk: desktop,
+            }],
+            lastFourTickets: [],
+        }));
         const ticketHandlerRepository = new TicketRepository();
 
         // When
-        const result = ticketHandlerRepository.addNewTicket();
+        const result = ticketHandlerRepository.assingTicket(desktop);
 
         // Then
         const expectedNewTicket: Ticket = {
             numb: 1,
-            desk: null,
+            desk: desktop,
         };
         expect(result).toEqual(expectedNewTicket);
     });
 
-    describe('Given a request to service a ticket', () => {
-        it('Should return a ticket with an assigned desk', () => {
-            // Given
-            const desktop = 0;
-            mockDate();
-            fs.readFileSync = jest.fn().mockImplementation(() => ({
-                latestTicket: 0,
-                today: 13,
-                tickets: [{
-                    numb: 1,
-                    desk: desktop,
-                }],
-                lastFourTickets: [],
-            }));
-            const ticketHandlerRepository = new TicketRepository();
+    it('Should return null if there are no tickets available to service', () => {
+        // Given
+        const desktop = 0;
+        mockDate(20);
+        const ticketHandlerRepository = new TicketRepository();
 
-            // When
-            const result = ticketHandlerRepository.assingTicket(desktop);
+        // When
+        const result = ticketHandlerRepository.assingTicket(desktop);
 
-            // Then
-            const expectedNewTicket: Ticket = {
-                numb: 1,
-                desk: desktop,
-            };
-            expect(result).toEqual(expectedNewTicket);
-        });
-
-        it('Should return null if there are no tickets available to service', () => {
-            // Given
-            const desktop = 0;
-            mockDate(20);
-            const ticketHandlerRepository = new TicketRepository();
-
-            // When
-            const result = ticketHandlerRepository.assingTicket(desktop);
-
-            // Then
-            expect(result).toBeNull();
-        });
+        // Then
+        expect(result).toBeNull();
     });
 });
+*/
