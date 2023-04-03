94b54481eaf494a5140fa05f067e3301ef99b2cb
ogomezga <osckar96@gmail.com>
2023-04-03 20:35:52 +0200

    feat(master): adding the repository to awilix registry

--- src/container.ts
+++ src/container.ts
@@ -1,10 +1,21 @@
-import { AwilixContainer, createContainer } from 'awilix';
+import { AwilixContainer, BuildResolver, Constructor, DisposableResolver, Lifetime, asClass, createContainer } from 'awilix';
+import { Dependencies } from './infrastructure/dependencies';
+import { TicketRepository } from './infrastructure/repositories/ticket-handler-repository';
 
-let container: AwilixContainer;
+let container: AwilixContainer<Dependencies>;
 
 export function getMainContainer() {
     if (!container) {
         container = createContainer();
+
+        container.register({
+            ticketHandlerRepository: asSingletonClass(TicketRepository),
+        });
     }
+
     return container;
+}
+
+function asSingletonClass<T>(type: Constructor<T>): BuildResolver<T> & DisposableResolver<T> {
+    return asClass(type, { lifetime: Lifetime.SINGLETON });
 }
