070f811ba6d348d7b1bde20194ea09b9fad1b775
ogomezga <osckar96@gmail.com>
2023-04-25 18:17:26 +0200

    feat(master): creating the new dependencies types

--- src/infrastructure/dependencies.ts
+++ src/infrastructure/dependencies.ts
@@ -1,12 +1,35 @@
 import { ServerFactory } from './server-factory';
 import TicketHandlerRepositoryInterface from '../domain/repositories/ticket-handler-repository-interface';
+import {NewTicketListener} from './listeners/new-ticket-listener';
+import {HandleTicketListener} from './listeners/handle-ticket-listener';
+import {PendingTicketsEmitter} from './emitters/pending-tickets-emitter';
+import {LastFourHandleTicketsEmitter} from './emitters/last-four-handle-tickets-emitter';
+import {LastTicketEmitter} from './emitters/last-ticket-emitter';
+import { Server } from 'socket.io';
+import {SocketClient} from './socket-client';
 
 type ServerDependencies = {
     serverFactory: ServerFactory;
+    socketServer: Server;
 };
+
+export type InfrastructureDependencies = {
+    socketClient?: SocketClient;
+} & ListenerDependencies & EmitterDependencies;
 
 type RepositoryDependencies = {
     ticketHandlerRepository: TicketHandlerRepositoryInterface;
 };
 
-export type Dependencies = ServerDependencies & RepositoryDependencies;
+type ListenerDependencies = {
+    newTicketListener?: NewTicketListener;
+    handleTicketListener?: HandleTicketListener;
+};
+
+type EmitterDependencies = {
+    lastTicketEmitter?: LastTicketEmitter;
+    pendingTicketsEmitter?: PendingTicketsEmitter;
+    lastFourHandleTicketsEmitter?: LastFourHandleTicketsEmitter;
+};
+
+export type Dependencies = ServerDependencies & RepositoryDependencies & InfrastructureDependencies;
