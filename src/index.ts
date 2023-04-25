4b87eaa70a8f9bcae983f53e43c9510153d216d8
ogomezga <osckar96@gmail.com>
2023-04-25 18:18:20 +0200

    feat(master): resolving the dependency instead of create an object and call the socket connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { ServerFactory } from './infrastructure/server-factory';
 import * as dotenv from 'dotenv';
+import {resolveDependency} from './infrastructure/dependency-injection';
 
 dotenv.config();
-const serverFactory = new ServerFactory({ port: process.env.PORT});
+const serverFactory: ServerFactory = resolveDependency('serverFactory');
 
-Promise.all([serverFactory.createServer(), serverFactory.start()])
-.then(() => console.info('Server started: ', serverFactory.getPort()))
+Promise.all([serverFactory.createServer({ port: process.env.PORT}), serverFactory.start()])
+.then(() => {
+    serverFactory.onSocketConnection();
+    console.info('Server started: ', serverFactory.getPort());
+})
 .catch((reason) => console.error('Error en in server: ', reason));
