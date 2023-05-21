8d685394ac66a247d47641741a7be59589a2ec79
ogomezga <osckar96@gmail.com>
2023-05-21 12:59:19 +0200

    feat(master): removing method from interface

--- src/domain/socket-client-interface.ts
+++ src/domain/socket-client-interface.ts
@@ -1,8 +1,5 @@
-import {Server} from 'socket.io';
 import {Ticket} from './models/ticket';
 
 export default interface SocketClientInterface {
-    registerSocketServer(socketServer: Server): void;
-
     emit({eventName, payload}: { eventName: string; payload: Ticket | Ticket[] | number }): void;
 }
