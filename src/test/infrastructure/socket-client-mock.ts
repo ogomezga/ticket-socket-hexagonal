054ec2a0a121f0f779ef9e8f66dfacf61a26e99a
ogomezga <osckar96@gmail.com>
2023-05-21 12:59:39 +0200

    feat(master): creating socket client mock

---
+++ src/test/infrastructure/socket-client-mock.ts
@@ -0,0 +1,18 @@
+import SocketClientInterface from '../../domain/socket-client-interface';
+import {Ticket} from '../../domain/models/ticket';
+
+export class SocketClientMock implements SocketClientInterface {
+    private socketServer = jest.fn();
+
+    emit({eventName, payload}: { eventName: string; payload: Ticket | Ticket[] | number }): void {
+        this.socketServer(eventName, payload);
+    }
+
+    assertEmitHaveBeenCalledTimes ( times: number ) {
+        expect(this.socketServer).toHaveBeenCalledTimes(times);
+    }
+
+    assertEmitHaveBeenCalledWith ( times: number, args: { eventName: string; payload: Ticket | Ticket[] | number } ) {
+        expect(this.socketServer).toHaveBeenNthCalledWith(times, args);
+    }
+}
